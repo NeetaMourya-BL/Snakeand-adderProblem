e30e9bbc00601b4116840bc8018e849d85845d06
NeetaMourya-BL <neeta.mourya@bridgelabz.com>
2023-01-04 17:27:02 +0530

    Added: The Player rolls the die to get a number between 1 to 6.

--- src/com/bridgelabz/snakeandladderproblem/SnakeandLadderProblem.java
+++ src/com/bridgelabz/snakeandladderproblem/SnakeandLadderProblem.java
@@ -23,6 +23,7 @@ public class SnakeandLadderProblem {
 	}
 
 	public static int diceRolls() {
+		// between 1 to 6. - Use ((RANDOM)) to get the number between 1 to 6
 		int diceResult = (int) (Math.floor(Math.random() * 10) % 6 + 1);
 		return diceResult;
 
