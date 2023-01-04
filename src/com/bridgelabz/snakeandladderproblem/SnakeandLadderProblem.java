c35ef9586ae6437c9ac04a98728bcaf14f7d65d7
NeetaMourya-BL <neeta.mourya@bridgelabz.com>
2023-01-04 17:12:30 +0530

    Welcome to Snake & Ladder Simulator Program

---
+++ src/com/bridgelabz/snakeandladderproblem/SnakeandLadderProblem.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.snakeandladderproblem;
+
+/*
+* This problem simulates a Snake and Ladder Game. The Player
+* starts from 0 rolls the die to get a number between 1 to 6, finds
+* a safe place, ladder or a snake keeps doing till the winning spot
+* 100 is achieved.
+*/
+
+//Snake & Ladder Simulator
+
+public class SnakeandLadderProblem {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Snake & Ladder Simulator Program");
+	}
+}
